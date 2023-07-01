0cf6b15af736cd2960b44a184ea65cf9a0051db9
Сергей <137560379+qa-brokek1d@users.noreply.github.com>
2023-07-01 18:06:40 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,10 +4,12 @@ function menuOnClick() {
   document.getElementById("menu-bg").classList.toggle("change-bg");
 }
 
-  var audio = new Audio('sound.wav');
-  audio.loop = true;
-  var playAudioBtn = document.getElementById('play-audio');
+var audio = new Audio('sound.wav');
+audio.loop = true;
+var playAudioBtn = document.getElementById('play-audio');
+var video = document.getElementById('background-video');
 
-  playAudioBtn.addEventListener('click', function() {
-    audio.play();
-  });
+playAudioBtn.addEventListener('click', function() {
+  audio.play();
+  video.play();
+});
